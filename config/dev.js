a92d53a8f3240bb90bfea1b968de285ff08354ec
assafperetz6 <assafperetz6@gmail.com>
2024-12-16 19:04:28 +0200

    Update the dev env as well

--- config/dev.js
+++ config/dev.js
@@ -1,4 +1,4 @@
 export default {
-    dbURL: 'mongodb+srv://assafperetz6:WUU9QAA75Pe3zN3D@cluster0.9hcy5.mongodb.net/',
-    dbName: 'toyDB',
+    dbURL: process.env.DB_URL,
+    dbName: process.env.DB_NAME,
 }
